3cce0c791fa7eb776e3a68cf3a01b25c124a0bdc
Shubham Sharma <shubhamtech004@gmail.com>
2023-11-03 23:17:13 +0530

    variable in c

---
+++ variable.c
@@ -0,0 +1,14 @@
+// variable is the name of a memory location which store some data;
+
+// ############ ---- implematation of variable ###################
+
+# include<stdio.h>
+  int main(){
+    int number = 25;
+    int age = 22;
+    char star = '*';
+    float pi = 3.145;
+
+    // printf("%d %d %c %f",age,number,star,pi);
+    return 0;
+  }
