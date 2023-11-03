e344d6286d529590ce4ca4fca60f224e84674e19
Shubham Sharma <shubhamtech004@gmail.com>
2023-11-03 23:55:48 +0530

    data type in c

---
+++ datatype.c
@@ -0,0 +1,74 @@
+/*
+Data type                                       size in byte
+
+cahr or signed char  -----------------------      1
+
+unsigned char    -------------------------        1
+
+int or signed int -----------------------         2
+
+unsigned int ------------------------------       2
+
+short int or unsigned short int  --------------    2
+
+signrd short int      ---------------------        2  
+
+long int or signed long int  ----------------      4
+
+float  -------------------------------             4
+
+double     ---------------------------             8
+
+long double     -----------------------            10
+
+*/
+
+# include<stdio.h>
+
+int main(){
+    // for integer data
+
+    int age = 23;
+
+    // for float value 
+
+    float pi = 3.89;
+
+    // for charcter value
+
+    char str = '#';
+
+
+    return 0;
+
+}
+
+// ******************* CONSTANT IN C *******************
+
+/*
+CONSTANT IN C 
+
+there are three type of constnt in c 
+
+1 integr constant (1,2,3,0,-1,-2);
+
+2 real conatnt (1.0,2.0,3.14,-24);
+
+3  charcter constant ('a', 'b', 'A', '#', '&');
+
+
+
+*/
+
+
+// ************************** KEYWORDS IN C *********************************
+
+/*
+
+KEYWORDS :- keywords are reserved words that have special meaning to the compiler..
+
+there are 32 keywords in c..
+
+
+
+*/
