6c88552b68275787c4ce4fd25658a7306b6d6eaf
Shubham Sharma <shubhamtech004@gmail.com>
2023-11-04 00:38:01 +0530

    question  in  c

---
+++ 4_instruction.c
@@ -0,0 +1,54 @@
+/*
+ INSTRUCTION IN C:-
+ these are statments in aprogram;
+
+ types :- there are three types of intruction 
+ 1 type declaration 
+ 2 airthmetic 
+ 3 control 
+
+
+ Example of valid statment :----------
+
+ int a = 22;
+ int b = a;  
+ int c = b +1;
+ int d = 1,e;
+ int a,b,c;
+
+ a=b=c=1;
+
+
+*/
+
+
+# include<stdio.h>
+
+int main(){
+   
+    /* 
+    type decelaration 
+    int x,y,z;
+    x=y=z=4;
+    printf("%d",x);
+
+    */
+
+   /*
+   AIRTHMATIC 
+
+    int a = 10, b= 2;
+   int sum = a+b;
+   int mul = a*b;
+   printf("%d \n",sum);
+   printf("%d \n" , mul);
+
+   */
+  
+
+
+
+
+    return 0;
+
+}
