15f53e5d44a90a8ca7ecbc89144e86430b64f054
Shubham Sharma <shubhamtech004@gmail.com>
2023-11-08 00:57:15 +0530

    control instruction in c

---
+++ 5_controlins.c
@@ -0,0 +1,20 @@
+/*
+CONTROL INSTRUCTION
+
+used to determine flow of program
+
+(i). Sequence control
+(ii). Decision Control
+(iii). Loop Control
+(iv). Case control
+
+***************** OPERATOR ******************
+(a). aithmatic operator
+(b). relational operator   <===========> (==, >, >=, <, <=, !=);
+(c). logical operator      <===========> (&&=>AND, ||=> OR , !=> NOT)
+(d). bitwise operator
+(e). assignment operator
+(f). ternary operator
+
+
+*/
