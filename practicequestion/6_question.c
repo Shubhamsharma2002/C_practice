e14998a375c846d513d0b665cf5cdf81ee8456db
Shubham Sharma <shubhamtech004@gmail.com>
2023-11-15 00:25:31 +0530

    check charater is uper or lower case c

---
+++ practicequestion/6_question.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("enter the value of ch : \n");
+    scanf("%c", &ch);
+    if(ch >= 'A' && ch <= 'Z'){
+        printf("uper case");
+
+    }
+    else if (ch >= 'a' && ch <= 'z')
+    {
+        printf("lowercase");
+    }else{
+        printf("not english");
+    }
+    return 0;
+}
