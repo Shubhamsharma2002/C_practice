631440581f412cc981c2020361e1878485153027
Shubham Sharma <shubhamtech004@gmail.com>
2023-11-16 01:30:27 +0530

    loops in c

---
+++ 7_loops.c
@@ -0,0 +1,31 @@
+/*
+LOOPS IN C
+
+lops are use to repate a task with condition
+types:-
+1 for loop 
+2 while loop
+3 do while loop
+
+syntax :-
+
+for loop:-
+
+for(initialisation,condition, updation){
+    // task
+}
+
+
+*/
+
+
+#include<stdio.h>
+int main(){
+
+    // for loop
+    for(int i = 0; i<=5;i++){
+        printf("%d \n",i);
+    }
+
+    return 0;
+}
